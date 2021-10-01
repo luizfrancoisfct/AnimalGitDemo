9547ee18e7afda6036e07b8f76a53f59eb75229b
Luiz <lp.araujo@campus.fct.unl.pt>
2021-10-01 10:25:50 +0100

    Initial version of our main class.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    /**
+     * @author Luiz Francois, n. 57188
+     * Demo for the Git lab on Software Engineering
+     */
+        public static void main(String[] args) {
+            System.out.println("Animal git demo");
+        }
+    }
